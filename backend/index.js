efab21d778868ab7890715f24f0f9e0bebb80a7d
Mukesh Kumar <mukeshk0326@gmail.com>
2022-01-19 14:28:18 +0530

    jwt-assignment

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const data = require("./Cities");
+const citySchema = require("./model/CitySchema");
+
+//   Connecting to the database..
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/citydb", {}).then(() => {
+  console.log("connected to database successfully..");
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("You are in homepage");
+});
+
+app.post("/login", (req, res) => {
+  const token = jwt.sign(req.body.userID, "get-jwt-token");
+  console.log(req);
+  res.json({
+    status: "Token generaged successfully..",
+    token: token,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server is listening at 3000..");
+});
